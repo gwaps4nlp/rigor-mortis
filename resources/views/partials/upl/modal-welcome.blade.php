84510bd6af2fc6e2f4c48d19df5cdb611378036e
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2018-05-18 13:16:34 +0200

    Fix message

--- resources/views/partials/upl/modal-welcome.blade.php
+++ resources/views/partials/upl/modal-welcome.blade.php
@@ -4,20 +4,17 @@
 	<div class="modal-body">
 		<img class="scientist" src="{{ asset('img/savant.jpg') }}" style="float:right;border-radius: 50px;width:80px;border: solid 2px black;" />
 			<p>
-			Bienvenue, explorateur ! Je suis le professeur Indianaperrier, aventurier de mon état.<br/>Lors d'une de mes récentes expéditions au coeur des pyramides, j'ai rencontré d’étranges phénomènes, et j'aurais bien besoin de ton aide pour en dévoiler tous les mystères.
+			Bienvenue, explorateur&nbsp;! Je suis le professeur Indianaperrier, aventurier de mon état.<br/>
+      Lors d'une de mes récentes expéditions au cœur des pyramides, j'ai rencontré d’étranges phénomènes, et j'aurais bien besoin de ton aide pour en dévoiler tous les mystères&nbsp;: nous cherchons des expressions multi-mots&nbsp;!
 			</p>
 			<p>
-			Si dans la partie classique de Zombilingo tu dois trouver des phénomènes bien précis, ici, ils pourront avoir des têtes bien différentes : nous cherchons des expressions multi-mots !
-			</p>
-			<p>
-			Ah ! Je dois partir pour le moment. Tu sais... l’aventure, tout ça. Mais je t’en prie, commence donc sans moi, je te rejoindrai au plus vite pour te donner plus d’indications.
+			Ah&nbsp;! Je dois partir pour le moment. Tu sais… l’aventure, tout ça. Mais je t’en prie, commence donc sans moi, je te rejoindrai au plus vite pour te donner plus d’indications.
 			</p>
 		<div class="modal-footer text-center">
-		<button type="button" class="btn btn-success closeNextLevel" data-dismiss="modal">{{ trans('site.continue') }}</button>  
+		<button type="button" class="btn btn-success closeNextLevel" data-dismiss="modal">{{ trans('site.continue') }}</button>
 		</div>
 	</div>
-
   </div>
-  
+
 </div>
 </div>

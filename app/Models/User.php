df63d655ec18b17eba07573f7eff03e21db8414d
Nicolas Lefebvre <nicolas@MacBook-Pro-de-Nicolas.local>
2019-11-29 17:27:37 +0100

    options export

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,5 +6,14 @@ use Gwaps4nlp\Core\Models\User as Gwaps4nlpUser;
 
 class User extends Gwaps4nlpUser
 {
-
+    /**
+    * Check if the user has a role
+    *
+    * @param Role $role
+    * @return boolean
+    */
+    public function hasRole($role): bool
+    {
+        return $this->roles->contains('id', $role->id);
+    }
 }
